ab5801eb336a732da510e0e6c0cf8320b5b38004
锦涛 <43426976+mathLjtao@users.noreply.github.com>
2020-05-12 13:59:45 +0800

    add-1

--- springboot-fastdfs/src/main/java/com/ljtao3/springbootfastdfs/controller/FileController.java
+++ springboot-fastdfs/src/main/java/com/ljtao3/springbootfastdfs/controller/FileController.java
@@ -1,5 +1,7 @@
 package com.ljtao3.springbootfastdfs.controller;
 
+import com.github.tobato.fastdfs.domain.proto.storage.DownloadByteArray;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
 import com.ljtao3.springbootfastdfs.config.FileDfsUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLEncoder;
 
 /**
  * @author ljtao3 on 2020/3/12
@@ -19,6 +24,8 @@ import javax.annotation.Resource;
 public class FileController {
     @Resource
     private FileDfsUtil fileDfsUtil;
+    @Resource
+    private FastFileStorageClient storageClient;
     /*
         http://localhost:9014/swagger-ui.html
         将返回的路径加上ip地址，就可以访问到上传的文件
@@ -42,6 +49,32 @@ public class FileController {
         }
         return ResponseEntity.ok(result);
     }
+
+    /*
+        官方FastDFS_Client框架说明：
+        https://github.com/tobato/FastDFS_Client
+        下载单个文件
+        https://www.jianshu.com/p/f440e495f309
+        // 获取文件元数据
+        Set<MateData> getMetadata(String groupName, String path);
+     */
+    @RequestMapping("/downloadFile/{id}")
+    public ResponseEntity<String> downloadFile(@PathVariable String id, HttpServletResponse response) throws IOException {
+        String filePath="";
+        String fileName= URLEncoder.encode("测试.txt","UTF8");
+        //DownloadByteArray callback=new DownloadByteArray();
+        //byte[] b = storageClient.downloadFile("gourp1", filePath, callback);
+
+        byte[] b=new byte[]{'a','b','c',94,95};
+        response.reset();
+        response.setContentType("application/x-download");
+        response.addHeader("Content-Disposition" ,"attachment;filename=\"" +fileName+ "\"");
+        response.getOutputStream().write(b);
+        response.getOutputStream().close();
+        return ResponseEntity.ok("");
+    }
+
+
     @GetMapping("/deleteByPath")
     public ResponseEntity<String> deleteByPath(String fileName){
         String filePathName=fileName;
